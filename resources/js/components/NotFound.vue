7da2ffafa96c216e8a2d4b2b1a436459ccb472fb
Yep <guillaume.moneret@gmail.com>
2020-08-26 16:36:41 +0200

    styling 404 page :lipstick:

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="my-5 text-center">
-        <h1 class="my-5">Not Found</h1>
+    <div class="my-5 text-center not-found">
+        <h1 class="my-5">Oups !</h1>
+        <h3 class="mb-4">Il semble que la page que vous avez demandée n'existe pas</h3>
+        <router-link to="/" tag="span">
+            <button class="button big-button d-flex p-4">
+                <div class="icon align-self-center mr-4"></div>
+                <div class="big-button-line-height"><strong>Retournez</strong> <br> <small>à l'accueil</small></div>
+            </button>
+        </router-link>
     </div>
 </template>
 
@@ -9,3 +16,9 @@
 
     }
 </script>
+
+<style>
+    .not-found button {
+        margin: 50px auto;
+    }
+</style>
