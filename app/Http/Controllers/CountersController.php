9ed1f2605346520ea813ba3d871cc98b4b117a7f
EvilYep <guillaume.moneret@gmail.com>
2021-01-12 14:14:54 +0100

    fixed bug incrementing counters :bug:

--- app/Http/Controllers/CountersController.php
+++ app/Http/Controllers/CountersController.php
@@ -10,7 +10,7 @@ class CountersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except('index');
+        $this->middleware('auth')->except(['index', 'increment']);
     }
 
     public function index()
